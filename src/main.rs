06931989b358cdf645433ab9b0a33bc0c67d2408
Saullo Bretas Silva <saullo.bretas55@gmail.com>
2022-10-01 19:41:14 -0300

    Added VideoDetails component

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,48 @@
 use yew::prelude::*;
 
+#[derive(Clone, PartialEq)]
+struct Video {
+    id: String,
+    title: String,
+    author: String,
+}
+
+#[derive(Clone, Properties, PartialEq)]
+struct VideoDetailsProps {
+    video: Video,
+}
+
+#[function_component(VideoDetails)]
+fn video_details(VideoDetailsProps { video }: &VideoDetailsProps) -> Html {
+    html! {
+        <div>
+            <h3>{format!("{} - {}", video.author.clone(), video.title.clone())}</h3>
+            <iframe id="player" type="text/html" width="600" height="400" src={format!("http://www.youtube.com/embed/{}?controls=0&origin=http://127.0.0.1", video.id.clone())} title={video.title.clone()} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=true></iframe>
+        </div>
+    }
+}
+
 #[function_component(App)]
 fn app() -> Html {
+    let videos = vec![Video {
+        id: "Yw6u6YkTgQ4".to_string(),
+        title: "hello world".to_string(),
+        author: "Louie Zong".to_string(),
+    }];
+
+    let videos = videos.iter().map(|video| {
+        html! {
+            <VideoDetails video={video.clone()}/>
+        }
+    });
+
     html! {
-        <h1>{"Hello world!"}</h1>
+        <>
+            <h1>{"Hello world videos!"}</h1>
+            <div>
+                {for videos}
+            </div>
+        </>
     }
 }
 
